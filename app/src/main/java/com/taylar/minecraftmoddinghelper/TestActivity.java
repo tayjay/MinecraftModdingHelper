dcb65ede4a1affa3a3384bd7d94fccbc0b13f84d
Taylar Masson <tay.j.masson@gmail.com>
2015-11-18 11:51:55 -0800

    Work on textview showing HTML syntax

--- app/src/main/java/com/taylar/minecraftmoddinghelper/TestActivity.java
+++ app/src/main/java/com/taylar/minecraftmoddinghelper/TestActivity.java
@@ -2,8 +2,17 @@ package com.taylar.minecraftmoddinghelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -11,6 +20,28 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        TextView tv = (TextView) findViewById(R.id.textView);
+        //tv.setMovementMethod(LinkMovementMethod.getInstance());
+        tv.setText(Html.fromHtml(readFile(R.raw.test)));
+    }
+
+    private String readFile(int id)
+    {
+        InputStream inputStream = getResources().openRawResource(id); //R.raw.test
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder total = new StringBuilder();
+        String line;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                total.append(line);
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        return total.toString();
     }
 
     @Override
